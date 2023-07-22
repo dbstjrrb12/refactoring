ea2b2584916872702c3ac64251667b02208e6da7
yoonseokgyu <dbstjrrb1@gmail.com>
2023-07-22 22:20:38 +0900

    [refactor] : 임시 변수 제거하기
    - format 함수를 별도 함수로 분리
    - 역할이 이름에 명확히 드러나도록 수정

--- refactoring/week1/statement.js
+++ refactoring/week1/statement.js
@@ -1,4 +1,5 @@
 import { amountFor } from './amountFor';
+import { usd } from './usd';
 import { playFor } from './playFor';
 import { volumeCreditsFor } from './volumeCreditsFor';
 
@@ -7,23 +8,17 @@ export function statement(invoice, plays) {
   let volumeCredits = 0;
   let result = `청구내역 (고객명: ${invoice.customer})\n`;
 
-  const format = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    maximumFractionDigits: 2,
-  }).format;
-
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
 
     // 청구 내역을 출력한다.
-    result += `${playFor(perf).name}: ${format(
-      amountFor(perf, playFor(perf)) / 100
-    )} ${perf.audience}석\n`;
+    result += `${playFor(perf).name}: ${usd(amountFor(perf, playFor(perf)))} ${
+      perf.audience
+    }석\n`;
 
     totalAmount += amountFor(perf, playFor(perf));
   }
-  result += `총액 ${format(totalAmount / 100)}\n`;
+  result += `총액 ${usd(totalAmount)}\n`;
   result += `적립 포인트 ${volumeCredits}점\n`;
 
   return result;
---
+++ refactoring/week1/usd.js
@@ -0,0 +1,7 @@
+export const usd = (aNumber) => {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    maximumFractionDigits: 2,
+  }).format(aNumber / 100);
+};
