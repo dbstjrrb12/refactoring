8d6c75f5d7b9e3766d734a0c91122bd5dccdf261
yoonseokgyu <dbstjrrb1@gmail.com>
2023-07-22 22:25:06 +0900

    [refactor] : credit 함수 쪼개기
    - credit 관련 반복문을 쪼갠 다음, 함수로 추출하여 인라인으로 사용

--- refactoring/week1/statement.js
+++ refactoring/week1/statement.js
@@ -5,12 +5,18 @@ import { volumeCreditsFor } from './volumeCreditsFor';
 
 export function statement(invoice, plays) {
   let totalAmount = 0;
-  let volumeCredits = 0;
   let result = `청구내역 (고객명: ${invoice.customer})\n`;
 
-  for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf);
+  const totalVolumeCredits = () => {
+    let volumeCredits = 0;
+    for (let perf of invoice.performances) {
+      volumeCredits += volumeCreditsFor(perf);
+    }
 
+    return volumeCredits;
+  };
+
+  for (let perf of invoice.performances) {
     // 청구 내역을 출력한다.
     result += `${playFor(perf).name}: ${usd(amountFor(perf, playFor(perf)))} ${
       perf.audience
@@ -18,8 +24,9 @@ export function statement(invoice, plays) {
 
     totalAmount += amountFor(perf, playFor(perf));
   }
+
   result += `총액 ${usd(totalAmount)}\n`;
-  result += `적립 포인트 ${volumeCredits}점\n`;
+  result += `적립 포인트 ${totalVolumeCredits()}점\n`;
 
   return result;
 }
