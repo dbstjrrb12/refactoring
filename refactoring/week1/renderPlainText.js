ed2732928c5412369c915d4437b54060872d58db
yoonseokgyu <dbstjrrb1@gmail.com>
2023-07-23 15:50:43 +0900

    [refactor] : 역할에 따라 함수 분리하기

---
+++ refactoring/week1/renderPlainText.js
@@ -0,0 +1,21 @@
+export default function renderPlainText(data) {
+  let result = `청구내역 (고객명: ${data.customer})\n`;
+
+  const usd = (aNumber) => {
+    return new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      maximumFractionDigits: 2,
+    }).format(aNumber / 100);
+  };
+
+  for (let perf of data.performances) {
+    // 청구 내역을 출력한다.
+    result += `${perf.play.name}: ${usd(perf.amount)} ${perf.audience}석\n`;
+  }
+
+  result += `총액 ${usd(data.totalAmount)}\n`;
+  result += `적립 포인트 ${data.totalVolumeCredits}점\n`;
+
+  return result;
+}
